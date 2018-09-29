9e72620e3a63dd20446de4cc7db1c22a25e8a3d3
vladovoev <vlad.v@leanst.com>
2018-09-29 17:24:33 +0300

    routes

--- app/packages/dabatase_service/routes/get.js
+++ app/packages/dabatase_service/routes/get.js
@@ -1,18 +1,47 @@
 
 const mongoose = require('mongoose');
 const Mail = mongoose.model('Mail')
-module.exports = server => {
-    server.get('/', async (_, res) => {
-        const m = new Mail({
-            subject: 'xaxa',
-            receiver: 'xaxa22',
-            content: 'qj xui'
-        })
 
-        await m.save();
+const pingHandler = (req, res) => {
+    res.send('Healthy')
+}
 
-        res.send('Worked');
+const mailHandler = async (req, res) => {
+    let mails, error;
+
+    try {
+        mails = await Mail.find();
+    } catch (error) {
+        error = error;
+    }
+
+    res.send({
+        message: 'Response from db',
+        ms: 'Database Service',
+        status: 200,
+        payload: mails || error
     })
+}
 
+const singleMailHandler = async (req, res) => {
+    const id = req.params.id;
+    let mail, error;
+    try {
+        mail = await Mail.findOne({ _id: id })
+    } catch (error) {
+        error = error;
+    }
 
+    res.send({
+        message: 'Response from db',
+        ms: 'Database Service',
+        status: 200,
+        payload: mail || error
+    })
+}
+module.exports = server => {
+    server
+        .get('/', pingHandler)
+        .get('/mails', mailHandler)
+        .get('/mails/:id', singleMailHandler)
 }
